a1b1a33e2b31af2bee8a9288c5d17a9d0d9cd2fd
bnorikane <bnorikane@msn.com>
2022-06-08 17:16:21 -0600

    Complete mobile nav
    - toggle mobile nav on click
    - close mobile nav after menu selection

--- js/script.js
+++ js/script.js
@@ -29,6 +29,36 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+//////////////////////////////////////////////
+// Smooth Scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if (href == "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to sections below
+    if (href != "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
